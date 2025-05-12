c54ea000367f023346e6362d677e629d59e8b99d
Pawan Singh Bhadouriya <135500641+Pawan-Singh-Bhadouriya@users.noreply.github.com>
2025-05-12 17:41:20 +0530

    Update DataBaseConnection.java

--- src/main/java/co/in/pricespy/dao/DataBaseConnection.java
+++ src/main/java/co/in/pricespy/dao/DataBaseConnection.java
@@ -9,10 +9,17 @@ public class DataBaseConnection {
 		// load the Driver
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		// getConnection
-		String url = "jdbc:mysql://localhost:3306/priceSpy";
-		String user = "root";
-		String pwd = "root";
+//		String url = "jdbc:mysql://localhost:3306/priceSpy";
+//		String user = "root";
+//		String pwd = "root";
+		
+		
+		String url = "jdbc:mysql://mysql-7904722-pawansingh2m4-5e71.h.aivencloud.com:19798/defaultdb?ssl-mode=REQUIRED";
+		String user = "avnadmin";
+		String pwd = "AVNS_xebAMf83jKk537843SD";
 
+
+		
 		Connection conn = DriverManager.getConnection(url, user, pwd);
 		System.out.println("Connection done");
 		return conn;
